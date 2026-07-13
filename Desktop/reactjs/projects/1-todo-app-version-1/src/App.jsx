37b7bbd20cd5e9c96b3717d1e234e70223c3f7ff
hetvingajeracodekaarigari <266034925+hetvingajera-afk@users.noreply.github.com>
2026-07-13 09:39:36 +0530

    Force fix case sensitivity pathways

--- Desktop/reactjs/projects/1-todo-app-version-1/src/App.jsx
+++ Desktop/reactjs/projects/1-todo-app-version-1/src/App.jsx
@@ -1,6 +1,6 @@
 import AppName from "./component/AppName";
-import AddToDo from "./component/AddToDo"
-import TodoItems from "./component/ToDoItems";
+import AddToDo from "./component/AddToDo";
+import TodoItems from "./component/TodoItems";
 import { useState } from "react";
 import "./App.css";
 function App() {
